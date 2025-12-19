590f877305405da415df6d2abe084b092ada83e7
ajaiprashanth.mardur <ajaiprashanth.mardur@gmail.com>
2025-12-19 17:47:01 +0000

    display problem

--- mitra_mobile/babel.config.js
+++ mitra_mobile/babel.config.js
@@ -3,7 +3,6 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin',
       [
         'module:react-native-dotenv',
         {
@@ -25,6 +24,7 @@ module.exports = function (api) {
           },
         },
       ],
+      'react-native-reanimated/plugin',
     ],
   };
 };
