ea72bc87aa92148317f45496349ebce5c2aa945a
Arya2022-mitra <ajaiprashanth.mardur@gmail.com>
2025-12-23 01:40:52 +0530

    Update MainApplication configuration

--- mitra_mobile/android/app/src/main/java/com/mitraveda/mitra_mobile/MainApplication.kt
+++ mitra_mobile/android/app/src/main/java/com/mitraveda/mitra_mobile/MainApplication.kt
@@ -23,8 +23,7 @@ class MainApplication : Application(), ReactApplication {
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+              // Packages that cannot be autolinked yet can be added manually here
             }
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
@@ -32,6 +31,9 @@ class MainApplication : Application(), ReactApplication {
           override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
           override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+          
+          // Disable bridgeless mode to resolve PlatformConstants missing error
+          override val isBridgelessEnabled: Boolean = false
       }
   )
 
